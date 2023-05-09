0e1d32b0773174d7bf450cb8bd7d932658575de3
ulisses-araujo <ulissesdeveloper82@gmail.com>
2023-05-09 20:54:44 -0300

    Implementando menu

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import modelo.entities.Menu;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		
+		Menu.menu();
 	}
 
 }
---
+++ src/modelo/entities/Menu.java
@@ -0,0 +1,36 @@
+package modelo.entities;
+
+import javax.swing.JOptionPane;
+
+public class Menu {
+	
+	public static void menu() {
+		Object[] menu = { "Conversor de Moeda", "Conversor de Temperatura" };
+		Object[] tipoMoedas = { "De Reais a Dólares", "De Reais a Euros", "De Reais a Libras",
+				"De Reais a Peso Argentinos", "De Reais a Peso Chilenos", "De Dólares a Reais", "De Euros a Reais",
+				"De Libras a Reais", "De Peso argentino a  Reais", "De Peso Chileno a Reais" };
+		
+		Object[] tipoTemperatura = { "Celcius para Farenheint", "Celcius para Kelvin", "Farenheint para Celcius",
+				"Farenheint para Kelvin", "Kelvin para Celcius", "Kelvin para Farenheint" };
+		
+		Object opcaoMenu = JOptionPane.showInputDialog(null, "Escolha uma opção", "Menu", -1, null, menu, "");
+		Double valor = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira um valor:", "Input", 3));
+		try {
+			if (opcaoMenu.equals(menu[0])) {
+				Object typeConvert = JOptionPane.showInputDialog(null, "Escolha a moeda para a qual você deseja girar seu dinheiro", "Moedas", -1, null, tipoMoedas, "");
+				
+			}else if (opcaoMenu.equals(menu[1])) {
+				Object typeTemperatura = JOptionPane.showInputDialog(null, "Escolha a temperatura a qual voçê deseja converter", "Temperatura", -1, null, tipoTemperatura, "");
+			}
+			
+			int resposta = JOptionPane.showConfirmDialog(null, "Deseja realizar outra conversão", "Conversão", JOptionPane.YES_NO_OPTION);
+			if (resposta == JOptionPane.YES_OPTION) {
+				menu();
+			}else {
+				JOptionPane.showMessageDialog(null, "Programa Finalizado");
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
